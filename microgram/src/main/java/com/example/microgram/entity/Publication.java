1daaf6509e22819513b3e8c8b82933e744eef164
Elnura Amanturova <elnura.amantur@gmail.com>
2023-03-13 17:57:55 +0600

    create new model Publication

---
+++ microgram/src/main/java/com/example/microgram/entity/Publication.java
@@ -0,0 +1,9 @@
+package com.example.microgram.entity;
+
+import java.time.LocalDateTime;
+
+public class Publication {
+    private String image;
+    private String description;
+    private LocalDateTime date;
+}
