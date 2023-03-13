c4fbdfe757480bffbf205a5e26e5e232c0a6ceab
Elnura Amanturova <elnura.amantur@gmail.com>
2023-03-13 17:58:54 +0600

    create new model Follow

---
+++ microgram/src/main/java/com/example/microgram/entity/Follow.java
@@ -0,0 +1,11 @@
+package com.example.microgram.entity;
+
+import java.time.LocalDateTime;
+
+public class Follow {
+
+    private User follower;
+    private User following;
+    private LocalDateTime date;
+
+}
