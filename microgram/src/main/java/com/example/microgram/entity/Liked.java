9727d98ec49c9fad7b4d5508c1543209ec84b93f
Elnura Amanturova <elnura.amantur@gmail.com>
2023-03-13 17:59:12 +0600

    create new model Liked with fields

---
+++ microgram/src/main/java/com/example/microgram/entity/Liked.java
@@ -0,0 +1,12 @@
+package com.example.microgram.entity;
+
+import java.time.LocalDateTime;
+
+public class Liked {
+    private User whoLiked;
+
+    private Publication publication;
+
+    private LocalDateTime date;
+
+}
