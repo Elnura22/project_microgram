86a566e4b6ad90a3cfe8eddd84205c76d7fab7c5
Elnura Amanturova <elnura.amantur@gmail.com>
2023-03-13 17:57:22 +0600

    create new class User with fields

---
+++ microgram/src/main/java/com/example/microgram/entity/User.java
@@ -0,0 +1,11 @@
+package com.example.microgram.entity;
+
+public class User {
+    private String name;
+    private String email;
+    private String password;
+    private Integer counterPublication;
+    private Integer counterFollower;
+    private Integer counterFollowing;
+
+}
