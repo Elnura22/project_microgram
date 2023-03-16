3e22a2f5d0e586df70c473ddd684ff2153dce145
Elnura Amanturova <elnura.amantur@gmail.com>
2023-03-16 18:51:03 +0600

    change these classes, add data annotation and ineteger field

--- microgram/src/main/java/com/example/microgram/entity/Liked.java
+++ microgram/src/main/java/com/example/microgram/entity/Liked.java
@@ -1,8 +1,12 @@
 package com.example.microgram.entity;
 
+import lombok.Data;
+
 import java.time.LocalDateTime;
 
+@Data
 public class Liked {
+    private Integer id;
     private User whoLiked;
 
     private Publication publication;
