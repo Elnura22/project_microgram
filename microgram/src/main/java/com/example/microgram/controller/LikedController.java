e0d98a299f38b746daa7027b8e082b8d41dd7211
Elnura Amanturova <elnura.amantur@gmail.com>
2023-03-26 16:55:26 +0600

    add methods in controllers

--- microgram/src/main/java/com/example/microgram/controller/LikedController.java
+++ microgram/src/main/java/com/example/microgram/controller/LikedController.java
@@ -1,16 +1,14 @@
 package com.example.microgram.controller;
 
+import com.example.microgram.dto.LikedDTO;
 import com.example.microgram.entity.Liked;
 import com.example.microgram.entity.Publication;
 import com.example.microgram.service.LikedService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -18,9 +16,15 @@ public class LikedController {
 
     private final LikedService service;
 
-    @GetMapping("/likedBy")
-    public ResponseEntity<List<Liked>> userByName(@RequestParam Publication publication){
-        return new ResponseEntity<>(service.getLikes(publication), HttpStatus.OK);
+//    @GetMapping("/likedBy")
+//    public ResponseEntity<List<Liked>> userByName(@RequestParam Publication publication){
+//        return new ResponseEntity<>(service.getLikes(publication), HttpStatus.OK);
+//    }
+
+    @PostMapping("/addLikeOnPublication")
+    public ResponseEntity<String> addLikeOnPublication(@RequestBody LikedDTO likedDTO) {
+        service.addLikeOnPublication(likedDTO);
+        return ResponseEntity.ok().build();
     }
 
 }
--- microgram/src/main/java/com/example/microgram/controller/PublicationController.java
+++ microgram/src/main/java/com/example/microgram/controller/PublicationController.java
@@ -1,32 +1,42 @@
 package com.example.microgram.controller;
 
-import com.example.microgram.entity.Follow;
-import com.example.microgram.entity.Publication;
-import com.example.microgram.entity.User;
+import com.example.microgram.dto.PublicationDTO;
+import com.example.microgram.dto.UserDTO;
 import com.example.microgram.service.PublicationService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/publications")
 public class PublicationController {
     private final PublicationService service;
 
-    @GetMapping("/publicationByUser")
-    public ResponseEntity<List<Publication>> userByName(@RequestParam int id){
-        return new ResponseEntity<>(service.getPublicationByUser(id), HttpStatus.OK);
+//    @GetMapping("/publicationByUser")
+//    public ResponseEntity<List<Publication>> userByName(@RequestParam Follow follow){
+//        return new ResponseEntity<>(service.getPublicationByFollowing(follow), HttpStatus.OK);
+//    }
+
+    @GetMapping("{id}")
+    public ResponseEntity<List<PublicationDTO>> getList(@PathVariable int id){
+        return new ResponseEntity<>(service.getOtherUsersPublications(id), HttpStatus.OK);
     }
 
-    @GetMapping("/publicationByUser")
-    public ResponseEntity<List<Publication>> userByName(@RequestParam Follow follow){
-        return new ResponseEntity<>(service.getPublicationByFollowing(follow), HttpStatus.OK);
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+    public PublicationDTO addPublication(@RequestBody PublicationDTO publicationData) {
+        return service.addPublication(publicationData);
     }
 
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deletePublication(@PathVariable Long id) {
+        if (service.deletePublication(id))
+            return ResponseEntity.noContent().build();
+        return ResponseEntity.notFound().build();
+    }
 
 }
