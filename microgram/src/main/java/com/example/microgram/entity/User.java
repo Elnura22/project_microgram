7ba83c0e101ad311ac709c29493a81272d0aa6e7
Elnura Amanturova <elnura.amantur@gmail.com>
2023-03-27 18:32:24 +0600

    change implements from UserDetails

--- microgram/src/main/java/com/example/microgram/entity/User.java
+++ microgram/src/main/java/com/example/microgram/entity/User.java
@@ -1,17 +1,21 @@
 package com.example.microgram.entity;
 
-import com.example.microgram.util.Generator;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
-@Data
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+@Getter
+@Setter
 @Builder
-@NoArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
-public class User {
+public class User implements UserDetails {
 
     private Long id;
     private String name;
@@ -25,4 +29,36 @@ public class User {
     @JsonProperty("counter_following")
     private int counterFollowing;
 
+    private String role;
+    private boolean enabled = Boolean.TRUE;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("FULL"));
+    }
+
+    @Override
+    public String getUsername() {
+        return getName();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
