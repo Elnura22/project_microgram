6f59d2cc0df38ff0092fde4db9ce757df742ea70
Elnura Amanturova <elnura.amantur@gmail.com>
2023-03-27 18:35:31 +0600

    add SecurityConfig

---
+++ microgram/src/main/java/com/example/microgram/configuration/SecurityConfig.java
@@ -0,0 +1,41 @@
+package com.example.microgram.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests().antMatchers("/publications/?").permitAll();
+
+        http.authorizeRequests().antMatchers("/publications/**").fullyAuthenticated();
+        http.authorizeRequests().antMatchers("/comments/**").fullyAuthenticated();
+        http.authorizeRequests().antMatchers("/addLikeOnPublication").fullyAuthenticated();
+        http.authorizeRequests().antMatchers("/followAction").fullyAuthenticated();
+
+
+        http.authorizeRequests()
+                .anyRequest()
+                .permitAll();
+
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        http.httpBasic();
+        http.formLogin().disable().logout().disable();
+        http.csrf().disable();
+    }
+
+}
