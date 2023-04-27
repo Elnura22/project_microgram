126cb6cefd1703b64754901f7f002ed422f972ac
Elnura Amanturova <elnura.amantur@gmail.com>
2023-04-27 18:22:15 +0600

    add method to SecurityConfig

--- microgram/src/main/java/com/example/microgram/configuration/SecurityConfig.java
+++ microgram/src/main/java/com/example/microgram/configuration/SecurityConfig.java
@@ -1,8 +1,10 @@
 package com.example.microgram.configuration;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -10,14 +12,27 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+import javax.sql.DataSource;
+
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+//    private final DataSource dataSource;
 
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
+
+//    @Autowired
+//    protected void configAuthentication(AuthenticationManagerBuilder auth) throws Exception {
+//        auth.jdbcAuthentication().passwordEncoder(new BCryptPasswordEncoder())
+//                .dataSource(dataSource)
+//                .usersByUsernameQuery("\"select email, password, enabled from customers where email?\"")
+//                .authoritiesByUsernameQuery("select email, role from customers where email?");
+//    }
+
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
